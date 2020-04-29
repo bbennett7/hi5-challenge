15d4733fbea77983b4e5bb2b36b42fb34d15c19b
bbennett7 <bryn.bennett@gmail.com>
2020-04-29 14:04:50 -0700

    App js file imports global styling and default layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
+import './styles/global.scss';
+import DefaultLayout from './containers/DefaultLayout/DefaultLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DefaultLayout />
     </div>
   );
 }
